0a7739081bf863b65414aa0557e881be79034ce6
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-05-07 08:13:19 +0200

    e20210507

--- src/main/java/org/crue/hercules/sgi/usr/config/DataConfig.java
+++ src/main/java/org/crue/hercules/sgi/usr/config/DataConfig.java
@@ -1,11 +1,8 @@
 package org.crue.hercules.sgi.usr.config;
 
-import org.crue.hercules.sgi.framework.data.domain.SgiAuditorAware;
 import org.crue.hercules.sgi.framework.data.jpa.repository.support.SgiJpaRepository;
-import org.springframework.context.annotation.Bean;
+import org.crue.hercules.sgi.framework.web.config.SgiDataConfig;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -16,10 +13,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaRepositories(basePackages = {
     "org.crue.hercules.sgi.usr.repository" }, repositoryBaseClass = SgiJpaRepository.class)
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
-public class DataConfig {
-  @Bean
-  public AuditorAware<String> auditorAware() {
-    return new SgiAuditorAware();
-  }
+public class DataConfig extends SgiDataConfig {
 }
